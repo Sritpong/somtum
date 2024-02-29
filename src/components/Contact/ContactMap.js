e2556fac01f6f16f52079835efc21ad925e50c18
Sritpong <jamebieskrub@gmail.com>
2024-03-01 00:48:52 +0700

    Update padding (contact pages)

--- src/components/Contact/ContactMap.js
+++ src/components/Contact/ContactMap.js
@@ -25,7 +25,7 @@ const ContactMap = () => {
                         }
                     }>
                         <Grid container>
-                            <Grid item xs={12} sm={12} md={6} p={3} height={300}>
+                            <Grid item xs={12} sm={12} md={6} p={3} height={450}>
                                 <iframe
                                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1937.1907459585398!2d100.64373887858062!3d13.816116197628094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d625e7cda81ab%3A0x72772242f4defcec!2s12%20Rachada-Ramintra%20Rd%2C%20Khwaeng%20Nuanchan%2C%20Khet%20Bueng%20Kum%2C%20Krung%20Thep%20Maha%20Nakhon%2010230!5e0!3m2!1sen!2sth!4v1709210723344!5m2!1sen!2sth"
                                     width="100%"
